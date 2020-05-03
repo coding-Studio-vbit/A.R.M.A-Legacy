4e7426e2ba067896a930892a967807efaa853f5b
saikiran <bsaikiran618@gmail.com>
2020-05-03 13:38:20 +0530

    Documented code

--- backend/node/users.js
+++ backend/node/users.js
@@ -1,12 +1,14 @@
+
 const {Client} = require('pg');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 
 
-async function generateAccessToken(data, secret, expirationTimeSeconds){
-	if(expirationDate == undefined) return jwt.sign(data,secret);
-	return jwt.sign(data,secret, {expiresIn: expirationTimeSeconds});
+async function generateAccessToken(data,secret,expirationTimeSeconds){
+
+	if(expirationDate == undefined) return jwt.sign(data,secret); //if no expiration date is specified, return token without expiration
+	return jwt.sign(data,secret, {expiresIn: expirationTimeSeconds}); //token with expiration.
 }
 
 
@@ -14,40 +16,65 @@ async function generateAccessToken(data, secret, expirationTimeSeconds){
 //if it is then it assigns the request a 'user' property.
 
 async function authenticateToken(request, res, next){ //use this as the middleware.
+	
+
+	//request must contain the "authorization" object in the header.
+	//the token is extracted from the authorization object.
+	
 	const header = request.headers['authorization'];
 	const token = header && header.split(' ')[1];
  
+
+ 	//if the token isn't present then send status code UNAUTHORIZED.
 	if(token == null) return res.sendStatus(401);
 
+	//verify the extracted token
 	jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,username)=>{
+
+		//if err then send status code FORBIDDEN
 		if(err) return res.sendStatus(403);
 
-		//check if the token is still in the validkeys.json file. >>>>>>	
+		//check if the token is still in the validkeys.json file.
+		const obj = fs.readFile('../validkeys.json', (err,data)=>{
+			if(err){
+				return res.sendStatus(500); // INTERNAL SERVER ERROR.
+			}
+			else
+			{
+				obj = JSON.parse(obj.toString());
+				
+				//check the validkeys object for the property with the same name as the username.
+				if(!(obj.hasOwnProperty(request.body.user.username))) return res.sendStatus(401); //if not found then send status code UNAUTHORIZED.
+			}
+		});
 
-		req.username = username;
-		next();
+		//here all the checks pass and the token is valid.
+		req.username = username; //set the username property to easily identify the user.
+		next(); //call the callback.
 	});
 }
+
+
 async function checkForumPassword(username,pwdhash,callback){
 
-	var client = new Client();
-	await client.connect();
+	var client = new Client(); 
+	await client.connect();//connect to DB
 
 	client.query("SELECT pwd_hash FROM FORUMS WHERE forum_name= $1 ;",[username],(err,res)=>{
 		if(err)
 		{
-			await client.end();
-			return callback(err,undefined);
+			client.end(); // kill the connection to DB
+			return callback(err,undefined); //return with error.
 		}
 		else
 		{
-			if(res.rowCount === 1 && res.rows[0].pwd_hash===pwdhash)
+			if(res.rowCount === 1 && res.rows[0].pwd_hash===pwdhash) //compare password hashes
 			{
-				await client.end();
-				return callback(undefined, true);
+				client.end();
+				return callback(undefined, true); //if match, return with true.
 			}
-			await client.end();
-			return callback(undefined,false);
+			client.end();
+			return callback(undefined,false); //if no match, then return false.
 		}
 	});
 }
@@ -60,17 +87,17 @@ async function checkRegistrationStatus(forum_name,callback){
 		client.query("SELECT forum_name FROM FORUMS WHERE forum_name= $1;",[forum_name],(err,res)=>{
 				if(err)
 				{
-					await client.end();
+					client.end();
 					return callback(err,null);
 				}
 				else
 				{
 					if(res.rowCount === 0){ 
-							await client.end();
+							client.end();
 							return callback(undefined,false);
 					}
 					else{
-							await client.end();
+							client.end();
 							return callback(undefined, true);
 					}
 				}
@@ -85,7 +112,7 @@ async function registerForum(forum_name, pwd_hash,email,phone,callback)
 
 		checkRegistrationStatus(forum_name,(err,res)=>{
 			if(res == true){
-				await client.end();
+				 client.end();
 				 return callback(undefined, false);
 			}
 			else{
@@ -93,12 +120,12 @@ async function registerForum(forum_name, pwd_hash,email,phone,callback)
 					
 					if(err)
 					{
-						await client.end();
+						client.end();
 						return callback(err,undefined);
 					}
 					else
 					{
-						await client.end();
+						client.end();
 						return callback(undefined, true);
 					}
 				});
@@ -109,5 +136,7 @@ async function registerForum(forum_name, pwd_hash,email,phone,callback)
 module.exports = {
 	checkForumPassword: checkForumPassword,
 	checkRegistrationStatus: checkRegistrationStatus,
-	registerForum:registerForum
+	registerForum:registerForum,
+	generateAccessToken: generateAccessToken,
+	authenticateToken: authenticateToken
 }
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,47 @@
+
+
+// PENDING TASKS ARE MARKED WITH A '>>>>>>' SIGN.
+
+
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const body_parser = require('body-parser');
-const users = require('node/users.js');
+const users = require('./node/users.js');
 const fs = require('fs');
-const port_number = process.env.PORT || 8080;
+const port_number = process.env.PORT || 8080; //PORT SPECIFIED IN THE .env file
 const app = express();
 
 
 //express configuration
 app.use(express.static(path.join(__dirname, "/public")));
-
 app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.urlencoded({ extended: false }));
 
 
+
+//HOME
 app.get('/',(req,res)=>{
-	res.sendFile('index.html');
+	
+	// GO TO HOME PAGE/ LOGIN PAGE >>>>>>>>>
+
 });
 
+
+//LOGIN VERIFICATION
+
 app.post('/login',(req,res)=>{ // checking password.
-	
+
+	console.log(req.body);
+
 	//Authenticate user. >>>>>>
 
 
 
 	//If login OK, Send access token.
+	//create access token.
 	const accessToken = users.generateAccessToken(req.body.user.username,process.env.SECRET_ACCESS_TOKEN) //no expiration time;
+	//send the access token.
 	res.send({accessToken: accessToken});
 	// now store access token in validkeys.json
 	const obj = fs.readFile('validkeys.json', (err,data)=>{
@@ -37,11 +52,19 @@ app.post('/login',(req,res)=>{ // checking password.
 	fs.writeFile('validkeys.json',JSON.strigify(obj),(err)=>{
 		console.log(err);
 		//redirect to login >>>>>
-	};)
+	});
 });
+
+//LOGOUT
+
 app.post('/logout',(req,res)=>{
 	
-		//get access token, authorize.
+	
+		//Extract access token from request and verify. >>>>>>
+		console.log(req.body);
+	
+
+
 		//if OK, remove the token from validkeys.json and redirect to login page.
 		const obj = fs.readFile('validkeys.json', (err,data)=>{
 		data = JSON.parse(data.toString());
@@ -49,14 +72,21 @@ app.post('/logout',(req,res)=>{
 		if(data.hasOwnProperty(req.body.user.username)){
 			delete  data[req.body.user.username];
 		}
-		else res.sendStatus(403); //forbidden. Cant logout without login.
+		else res.sendStatus(401); //UNAUTHORIZED. Can't logout without login.
 	})
 	//save the file.
 	fs.writeFile('validkeys.json', JSON.stringify(obj),(err)=>{
 		console.log(err);
 	})
 });
+
+
+//DASHBOARD
+
 app.get('/dashboard',users.authenticateToken, (req, res)=>{ //users.authenticateToken is the middleware.
+
+	//token verification done by middleware.
+	console.log(req.body);
 
 	//token OK
 	//redirect to dashboard of req.user . >>>>>>>>
@@ -64,21 +94,32 @@ app.get('/dashboard',users.authenticateToken, (req, res)=>{ //users.authenticate
 
 	res.json({msg: 'login success!'}); //TEST MESSAGE.
 });
+
+
+//REGISTRATION STATUS CHECK
+
+
 app.post('/checkRegistrationStatus',(req,res)=>{ 
 
-
 	//check user registered or not. >>>>>>>
+
 	console.log(req.body);
-	res.send({msg: 'checking registration status'});
+	res.send({msg: 'checking registration status'}); //TEST MESSAGE.
 });
+
+
+//REGISTER FORUM
+
 app.post('/registerForum',(req,res)=>{ 
 
 	// register a forum. >>>>>>>
 
 	console.log(req.body);
-	res.send({msg:'registered user.'});
+	res.send({msg:'registered user.'}); // TEST MESSAGE
 });
 
+
+//start the server.
 app.listen(port_number, ()=>{
 	console.log('server up and running on port:' + String(port_number));
 })
