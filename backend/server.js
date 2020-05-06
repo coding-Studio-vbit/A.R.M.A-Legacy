9498025c26ed0d41902db34c71fc7e392c4d5d2b
abhiram2600 <abhiram2600@users.noreply.github.com>
2020-05-06 11:54:41 +0530

    f

--- backend/server.js
+++ backend/server.js
@@ -19,89 +19,57 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-<<<<<<< HEAD
-  //check password >>>>>
-
-  console.log(req.body);
-
-  // //If login OK, Send access token.
-  // //create access token.
-  var access = "";
-
-  const accessToken = users
-    .generateAccessToken(
+  //check password
+  users
+    .checkForumPassword(
       req.body.user.username,
-      process.env.SECRET_ACCESS_TOKEN
+      req.body.user.password,
+
+      (err, status) => {
+        if (err) {
+          console.log(err);
+          return res.status(401).send({ message: err });
+        }
+
+        if (status == true) {
+          const accessToken = users.generateAccessToken(
+            req.body.user.username,
+            process.env.SECRET_ACCESS_TOKEN
+          );
+
+          res.send({ message: "Login Successful", accessToken: accessToken });
+          console.log("token: " + accessToken);
+
+          var obj = fs.readFileSync("./validkeys.json");
+          obj = obj.toString();
+          obj = JSON.parse(obj);
+          obj[req.body.user.username] = accessToken;
+
+          //save the data.
+          fs.writeFileSync("validkeys.json", JSON.stringify(obj));
+        } else {
+          res.status(401).send({ message: "Invalid Password" }); //password wrong, return UNAUTHORIZED.
+        }
+      }
     )
-    .then((ac) => {
-      res.send({ accessToken: ac });
-      access = ac;
-    })
-    .catch((err) => console.log(err)); //no expiration time;
-  //send the access token.
-  // now store access token in validkeys.json
-  const obj = fs.readFile("validkeys.json", (err, data) => {
-    if (data.toString() === "undefined") {
-      const temp = {};
-      temp[req.body.user.username] = accessToken;
-      return temp;
-    }
-    console.log(data.toString());
-    console.log(obj);
-    data = JSON.parse(data.toString());
-    data[req.body.user.username] = accessToken; //save access token with the username
-    return data;
-  });
-  //save the data.
-  fs.writeFile("validkeys.json", JSON.stringify(obj), (err) => {
-    console.log(err);
-    //redirect to login >>>>>
-  });
-=======
-
-	//check password 
-  	users.checkForumPassword(req.body.user.username,req.body.user.password,(err,status)=>{
-    	if(err){
-	 		 console.log(err);
-     		 return res.status(401).send({message: err});
-   		}
-
-	  if(status == true)
-	  {
-      	const accessToken = users.generateAccessToken(req.body.user.username,process.env.SECRET_ACCESS_TOKEN);
-    
-      	res.send({message:'Login Successful',accessToken: accessToken});
-      	console.log('token: '+accessToken);
-      	
-      	var obj = fs.readFileSync("./validkeys.json");
-      	obj = obj.toString();
-      	obj = JSON.parse(obj);
-      	obj[req.body.user.username] = accessToken;
-
-      	//save the data.
-      	fs.writeFileSync("validkeys.json",JSON.stringify(obj));
-	  }
-	  else
-	  {
-	  	res.status(401).send({message: "Invalid Password"}); //password wrong, return UNAUTHORIZED.
-	  }
-  }).catch((error)=>{console.log(error);res.status(500).send("Internal Server Error");})
->>>>>>> 152486f35305f0d7b41593be02b964ee54442e65
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 //LOGOUT
 
 app.post("/logout", (req, res) => {
-
   //if OK, remove the token from validkeys.json and redirect to login page.
   var obj = fs.readFileSync("validkeys.json");
-    obj = JSON.parse(obj.toString());
-    
-	if (obj.hasOwnProperty(req.body.user.username)) {
-      delete obj[req.body.user.username];
-    } else res.status(401).send({message: "CANNOT LOGOUT WITHOUT LOGIN!"}); //UNAUTHORIZED. Can't logout without login.
+  obj = JSON.parse(obj.toString());
 
-   res.send({message: "LOGOUT SUCCESSFUL!"})
+  if (obj.hasOwnProperty(req.body.user.username)) {
+    delete obj[req.body.user.username];
+  } else res.status(401).send({ message: "CANNOT LOGOUT WITHOUT LOGIN!" }); //UNAUTHORIZED. Can't logout without login.
+
+  res.send({ message: "LOGOUT SUCCESSFUL!" });
   //save the file.
   fs.writeFileSync("validkeys.json", JSON.stringify(obj));
 });
