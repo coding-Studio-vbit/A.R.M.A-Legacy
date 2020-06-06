ed5090859b7eba06cab7010ecd6644b72b8d0383
yasaswiraj <47531308+yasaswiraj@users.noreply.github.com>
2020-06-06 16:42:21 +0530

    clean slate

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const users = require('./node/users.js');
-
-users.registerForum('SSVFORUM','1234','blehbleh@blehmail.com','9999999999')
-.then(state=>{
-	console.log(state)
-}).catch((err)=>{
-	console.log(err);
-})
