052d23387b0a6042fc8b2aa9c100fa6c6aa385e5
yasaswiraj <47531308+yasaswiraj@users.noreply.github.com>
2020-06-06 15:59:47 +0530

    deleted :dab:

--- backend/test.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const users = require('./node/users.js');
-
-users.registerForum('StuTalk','1234','mailme@kunaldubey.com','9999999999','StuTalk',(err, state)=>{
-    console.log(err||state);
-})
