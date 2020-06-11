df3e7c816a64d868fa263b56b4074aabeca56e0c
saikiran <bsaikiran618@gmail.com>
2020-06-11 16:41:00 +0530

    test.js commit

--- backend/test.js
+++ backend/test.js
@@ -7,3 +7,14 @@ users.registerFaculty("Saravanan","18P61A05J1","CSE","sar11@sarvi.com","99999999
 }).catch(error=>{
 	console.log(error);
 })
+
+/*
+	//user this to register forums.
+	
+	users.registerForum("coding.Studio();","passwordpassword", "geemail@gmail.com", "9999999999" )
+	.then(state=>{
+		console.log("Registration status:" + state);
+	}).catch(error=>{
+		console.log("Error occured: "+error);
+	})
+*/
