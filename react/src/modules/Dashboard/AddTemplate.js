1ab8d874431c23594384952da310b1ceedcf48b2
abhiram2600 <abhiram2600@users.noreply.github.com>
2020-06-12 16:09:59 +0530

    upload functionality

--- react/src/modules/Dashboard/AddTemplate.js
+++ react/src/modules/Dashboard/AddTemplate.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Navi";
-import "./css/template.css";
+import "./css/inputfile.scss";
+import "./css/space.css";
+import axios from "axios";
 const AddTemplate = () => {
+  const [file, uploadFile] = useState("");
+  const [filename, setfilename] = useState(false);
+  const [response, setResponse] = useState("");
+
+  const handleChange = (e) => {
+    uploadFile(e.target.files[0]);
+    setfilename(true);
+    const formData = new FormData();
+    formData.append("file", file);
+    console.log(formData);
+    axios
+      .post(`${process.env.REACT_APP_URL}/uploadTemplate`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <Nav />
@@ -11,16 +36,19 @@ const AddTemplate = () => {
           paddingTop: 50,
         }}
       >
-        <div style={{ backgroundcolor: "white" }}>
-          <input
-            type="file"
-            style={{
-              color: "black",
-              backgroundColor: "white",
-              padding: 10,
-            }}
-          />
+        <h3 style={{ color: "white" }}>
+          upload your personalized letter template
+        </h3>
+        <br />
+        <div className="fexi" style={{ display: "block" }}>
+          <input type="file" id="file" onChange={handleChange} />
+          <label for="file" class="btn-2">
+            upload
+          </label>
+
+          <h4>{response}</h4>
         </div>
+        <br />
       </div>
     </div>
   );
