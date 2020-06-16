737e5b2183de1c89dc933a14f454ffd9a0aa2a11
yasaswiraj <47531308+yasaswiraj@users.noreply.github.com>
2020-06-17 00:50:22 +0530

    update a mistake

--- react/src/modules/Auth/FacultyRegistration.js
+++ react/src/modules/Auth/FacultyRegistration.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "./css/images/logo.png";
+import logo from "./images/logo.png";
 import axios from "axios";
 import "./css/Form.css";
 import "./css/registrationpage.css";
@@ -69,110 +69,138 @@ const FacultyRegister = () => {
   return (
     <div className="register-forms">
       <form>
-        <div style={{textAlign:"center"}}>
+        <div style={{ textAlign: "center" }}>
           <img
             src={logo}
             alt="logo"
-            style={{ width: "150px", height: "150px"}}
+            style={{ width: "150px", height: "150px" }}
           />
         </div>
-        <h1 style={{ color: "white",textAlign:"center" }}> A.R.M.A Faculty Registration</h1>
-        <br/>
-        <br/>
+        <h1 style={{ color: "white", textAlign: "center" }}>
+          {" "}
+          A.R.M.A Faculty Registration
+        </h1>
+        <br />
+        <br />
         <div className="container">
           <div className="row registration-row">
-            <div className="col-md registration-text"><h4 style={{paddingTop:"3%" }}>Roll No : </h4></div>
-            <div className="col-md"><input
-              type="text"
-              onChange={handleChange}
-              name="rollNo"
-              className="inputboxes"
-              value={contact.rollNo}
-              placeholder="Roll Number"
-            /></div>
+            <div className="col-md registration-text">
+              <h4 style={{ paddingTop: "3%" }}>Roll No : </h4>
+            </div>
+            <div className="col-md">
+              <input
+                type="text"
+                onChange={handleChange}
+                name="rollNo"
+                className="inputboxes"
+                value={contact.rollNo}
+                placeholder="Roll Number"
+              />
+            </div>
           </div>
           <div className="row registration-row">
-            <div className="col-md registration-text"><h4 style={{paddingTop:"3%" }}>Name : </h4></div>
-            <div className="col-md"><input
-              type="text"
-              onChange={handleChange}
-              name="name"
-              className="inputboxes"
-              value={contact.name}
-              placeholder="Name"
-            /></div>
+            <div className="col-md registration-text">
+              <h4 style={{ paddingTop: "3%" }}>Name : </h4>
+            </div>
+            <div className="col-md">
+              <input
+                type="text"
+                onChange={handleChange}
+                name="name"
+                className="inputboxes"
+                value={contact.name}
+                placeholder="Name"
+              />
+            </div>
           </div>
           <div className="row registration-row">
-            <div className="col-md registration-text"><h4 style={{paddingTop:"3%" }}>Department : </h4></div>
-            <div className="col-md"><select
-              className="login-dropdown round"
-              name="value"
-              onChange={(e) => setDept(e.target.value)}
+            <div className="col-md registration-text">
+              <h4 style={{ paddingTop: "3%" }}>Department : </h4>
+            </div>
+            <div className="col-md">
+              <select
+                className="login-dropdown round"
+                name="value"
+                onChange={(e) => setDept(e.target.value)}
+              >
+                {deptList.map((depts) => (
+                  <option> {depts} </option>
+                ))}
+              </select>
+
+              <span className="select-arrow"></span>
+            </div>
+          </div>
+          <div className="row registration-row">
+            <div className="col-md registration-text">
+              <h4 style={{ paddingTop: "3%" }}>Email : </h4>
+            </div>
+            <div className="col-md">
+              <input
+                type="email"
+                onChange={handleChange}
+                name="email"
+                className="inputboxes"
+                value={contact.email}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="row registration-row">
+            <div className="col-md registration-text">
+              <h4 style={{ paddingTop: "3%" }}>Confirm email : </h4>
+            </div>
+            <div className="col-md">
+              <input
+                type="email"
+                onChange={handleChange}
+                name="cemail"
+                className="inputboxes"
+                value={contact.cemail}
+                placeholder="Confirm Email"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <h5
+              style={{
+                display: !isEnabled ? "inline" : "none",
+                color: "#ff1744",
+              }}
+              id="emailHelp"
+              className="form-text"
             >
-              {deptList.map((depts) => (
-                <option> {depts} </option>
-              ))}
-            </select>
-
-            <span className="select-arrow"></span></div>
+              Enter the same email as above
+            </h5>
           </div>
           <div className="row registration-row">
-            <div className="col-md registration-text"><h4 style={{paddingTop:"3%" }}>Email : </h4></div>
-            <div className="col-md"><input
-              type="email"
-              onChange={handleChange}
-              name="email"
-              className="inputboxes"
-              value={contact.email}
-              placeholder="Email"
-            /></div>
+            <div className="col-md registration-text">
+              <h4 style={{ paddingTop: "3%" }}>Phone number : </h4>
+            </div>
+            <div className="col-md">
+              <input
+                type="text"
+                onChange={handleChange}
+                className="inputboxes"
+                name="pnum"
+                value={contact.pnum}
+                placeholder="Phone Number"
+              />
+            </div>
           </div>
-          <div className="row registration-row">
-            <div className="col-md registration-text"><h4 style={{paddingTop:"3%" }}>Confirm email : </h4></div>
-            <div className="col-md"><input
-              type="email"
-              onChange={handleChange}
-              name="cemail"
-              className="inputboxes"
-              value={contact.cemail}
-              placeholder="Confirm Email"
-            /></div>
-          </div>
-          <div className="row"><h5
-            style={{
-              display: !isEnabled ? "inline" : "none",
-              color: "#ff1744",
-            }}
-            id="emailHelp"
-            className="form-text"
-          >
-            Enter the same email as above
-          </h5></div>
-          <div className="row registration-row">
-            <div className="col-md registration-text"><h4 style={{paddingTop:"3%" }}>Phone number : </h4></div>
-            <div className="col-md"><input
-              type="text"
-              onChange={handleChange}
-              className="inputboxes"
-              name="pnum"
-              value={contact.pnum}
-              placeholder="Phone Number"
-            /></div>
-          </div>
-
         </div>
 
         <div>
-        <br/>
-        <button
-          type="submit"
-          className="buttonpurple"
-          onClick={handleRegister}
-        >
-          Register
-        </button>
+          <br />
+          <button
+            type="submit"
+            className="buttonpurple"
+            onClick={handleRegister}
+          >
+            Register
+          </button>
         </div>
-        <br/>
+        <br />
         <h4 style={{ color: isMessage ? "green" : "#ff1744" }}>{error} </h4>
       </form>
     </div>
